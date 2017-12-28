466d9608d1017c1efe2b7143a1bfd8f9a501efa0
Clement Udensi <cklemzey@gmail.com>
2017-12-28 23:38:05 +0100

    fixing internal error of db.collection is not a function

--- server2.js
+++ server2.js
@@ -18,6 +18,25 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 let db;
 
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
+
+    // Initialize the app.
+    const server = app.listen(process.env.PORT || 8080, function () {
+        const port = server.address().port;
+        console.log("App now running on port", port);
+    });
+});
+
+
 function handleError(res, reason, message, code) {
     console.log("ERROR: " + reason);
     res.status(code || 500).json({"error": message});
@@ -55,23 +74,5 @@ app.post("/contacts", function(req, res) {
         } else {
             res.status(201).json(doc.ops[0]);
         }
-    });
-});
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    // Save database object from the callback for reuse.
-    db = database;
-    console.log("Database connection ready");
-
-    // Initialize the app.
-    const server = app.listen(process.env.PORT || 8080, function () {
-        const port = server.address().port;
-        console.log("App now running on port", port);
     });
 });
